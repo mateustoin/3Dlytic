fb45aaf1cb4cecddfc3f92fc89f16f6636222611
Mateus Antonio <mateustoin@gmail.com>
2024-11-21 12:05:25 -0300

    Pylint refactor

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Entry point for the 3Dlytic application.
+Run this script to start the application.
+
+Usage: $ python main.py
+"""
 import sys
 from PySide6.QtWidgets import QApplication
 from controllers.main_controller import MainController
@@ -5,19 +11,25 @@ from controllers.main_controller import MainController
 def load_stylesheet():
     """Load the QSS stylesheet."""
     try:
-        with open("resources/styles.qss", "r") as file:
+        with open("resources/styles.qss", "r", encoding="utf-8") as file:
             return file.read()
     except FileNotFoundError:
         print("Stylesheet file not found.")
         return ""
 
 def main():
+    """
+    Main entry point for the 3Dlytic application.
+
+    This function initializes the QApplication, loads the application stylesheet,
+    creates the main controller, and starts the application event loop.
+    """
     app = QApplication(sys.argv)
-    
+
     # Load and apply the stylesheet
     stylesheet = load_stylesheet()
     app.setStyleSheet(stylesheet)
-    
+
     controller = MainController()
     controller.show()
     app.exec()
